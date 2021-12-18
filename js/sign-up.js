8e095ac11767d2a765b84d2a670348cc7c674c31
Thaís Barros Alvim <thaisbarrosalvim@gmail.com>
2021-12-18 16:45:01 -0300

    sign-up with creation of user in API

--- js/sign-up.js
+++ js/sign-up.js
@@ -4,6 +4,40 @@ Thaís Barros Alvim - RA: 2020008082
 Thiago Henrique Cruz de Moura - RA: 2020023875
 */
 
-function SignUp() {
-  window.location.href = "/index.html";
+async function CreateUser(name, pass, mail, image) {
+  let resp = await fetch("http://127.0.0.1:8000/code-book/api/create_user", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      username: name,
+      password: pass,
+      email: mail,
+      profile_image: image,
+    }),
+  }).then((resp) => {
+
+      return resp;
+    
+  })
+    .catch((e) => console.log("ERRO12" + e));
+  
+  console.log(await resp);
+  return resp;
+}
+
+async function SignUp() {
+  console.log("SignUp function!");
+  pass = document.getElementById("InputPassword").value;
+  user = document.getElementById("InputUsername").value;
+  iemail = document.getElementById("InputEmail").value;
+
+  const resp = await CreateUser(user, pass, iemail, null);
+  if (resp.ok) {
+    window.location.href = "/index.html";
+  } else {
+    console.log(resp);
+    alert("Erro ao logar: " + resp.statusText);
+  }
 }
